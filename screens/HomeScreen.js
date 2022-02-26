92b27f7ccf7d80b1fa141b8bddd13b71d529fe1e
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-26 18:20:41 +0100

    do something with tailwind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import {Text, View, StyleSheet} from 'react-native';
 import {SafeAreaView} from "react-native-safe-area-context";
+import tw from "twrnc";
 
 function Banner() {
     return (
@@ -13,21 +14,17 @@ export function HomeScreen() {
     return (
         <SafeAreaView>
             <Banner/>
+            <View style={tw`h-full bg-yellow-300`}>
+                <View style={tw`w-20 h-30 bg-red-400 border-2 border-red-800 m-3`}/>
+                <View style={tw`w-20 h-30 bg-green-400 border-2 border-green-800 m-3`}/>
+            </View>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create(
     {
-        banner: {
-            backgroundColor: "purple",
-            padding: 25,
-        },
-        bannerText: {
-            color: "white",
-            textAlign: "center",
-            fontSize: 20,
-            fontWeight: "bold"
-        }
+        banner: tw`bg-purple-700 p-5`,
+        bannerText: tw`text-white text-center text-lg`,
     }
 )
