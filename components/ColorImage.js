29a8e8527d34e99df68f876f2a65f72dbcd5ad00
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-26 18:39:07 +0100

    ColorImage v3

--- components/ColorImage.js
+++ components/ColorImage.js
@@ -4,10 +4,12 @@ import tw from 'twrnc';
 export function ColorImage() {
     return (
         <View style={tw`flex-1 bg-blue-100`}>
-            <View style={{width: 50, height: 50, backgroundColor: "pink"}}/>
-            <View style={{width: 100, height: 50, backgroundColor: "orange"}}/>
-            <View style={{width: 200, height: 50, backgroundColor: "coral"}}/>
-            <View style={{width: 150, height: 50, backgroundColor: "bisque"}}/>
+            <View style={tw`flex-row flex-1 bg-pink-100`}>
+                <View style={{width: 50, height: "100%", backgroundColor: "pink"}}/>
+                <View style={{width: 100, height: "100%", backgroundColor: "orange"}}/>
+                <View style={{width: 200, height: "100%", backgroundColor: "coral"}}/>
+                <View style={{width: 150, height: "100%", backgroundColor: "bisque"}}/>
+            </View>
             <View style={{flex: 1, width: 100, height: 50, backgroundColor: "lightsalmon"}}/>
             <View style={{flex: 1, width: 50, height: 50, backgroundColor: "hotpink"}}/>
             <View style={{flex: 1, width: "25%", height: 50, backgroundColor: "moccasin"}}/>
