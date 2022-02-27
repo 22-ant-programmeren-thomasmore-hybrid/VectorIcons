3c271a93bbefac7ccb7f365692de0b5694b4ace3
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-27 15:51:20 +0100

    FlatList step 2

--- screens/PlacesScreen.js
+++ screens/PlacesScreen.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {Icon} from "react-native-elements";
 import tw from "twrnc";
 import {usePlacesContext} from "../contexts/PlacesContext";
@@ -9,7 +9,7 @@ function PlaceIcon({place}) {
               color={place.color}
               type="ionicon"
               size={24}
-              style={styles.icon}/>
+              style={styles.placeIcon}/>
     );
 
 }
@@ -38,8 +38,12 @@ export function PlacesScreen() {
     const {places} = usePlacesContext();
     return (
         <View style={styles.container}>
-            {places.map(place =>
-                <Place key={place.id} place={place}/>)}
+            <FlatList
+                style={styles.flatlist}
+                data={places}
+                keyExtractor={place => place.id}
+                renderItem={({item}) => <Place place={item}/>}
+            />
         </View>
     );
 }
@@ -48,11 +52,12 @@ export function PlacesScreen() {
 const styles = StyleSheet.create(
     {
         container: tw`h-full bg-yellow-100`,
+        flatlist: tw`mb-3`,
         placeContainer: tw`w-full`,
         hairline: {height: StyleSheet.hairlineWidth, backgroundColor: "gray"},
         touchable: (isSelected) => tw`p-3 flex-row items-center ${isSelected ? "bg-orange-100" : ""}`,
         name: tw`font-semibold text-lg`,
         description: tw`text-gray-500`,
-        icon: tw`mr-4 bg-pink-100 rounded-full p-3`,
+        placeIcon: tw`mr-4 bg-pink-100 rounded-full p-3`,
     }
 )
