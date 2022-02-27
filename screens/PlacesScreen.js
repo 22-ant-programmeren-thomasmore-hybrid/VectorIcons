8ad54995a35b0127cd8031840e3563549cf6a385
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-27 14:25:37 +0100

    places screen step 2

--- screens/PlacesScreen.js
+++ screens/PlacesScreen.js
@@ -3,18 +3,34 @@ import tw from "twrnc";
 import {usePlacesContext} from "../contexts/PlacesContext";
 
 
-export function PlacesScreen() {
-    const {places} = usePlacesContext();
+export function Place({place}) {
     return (
-        <View style={styles.container}>
-            <Text >Places screen</Text>
-            {places.map(place => <Text key={place.id}>{place.name}</Text>)}
+        <View style={styles.placeContainer}>
+            <View style={styles.hairline}/>
+            <View style={tw`p-3`}>
+                <Text style={tw`font-semibold text-lg`}>{place.name}</Text>
+                <Text style={tw`text-gray-500`}>{place.description}</Text>
+            </View>
         </View>
     );
 }
 
+export function PlacesScreen() {
+    const {places} = usePlacesContext();
+    return (
+        <View style={styles.container}>
+            {places.map(place =>
+                <Place key={place.id} place={place}/>)}
+        </View>
+    );
+}
+
+// noinspection JSSuspiciousNameCombination
 const styles = StyleSheet.create(
     {
         container: tw`h-full bg-yellow-100`,
+        placeContainer: tw`w-full`,
+        hairline: {height: StyleSheet.hairlineWidth, backgroundColor: "gray"}
+
     }
 )
