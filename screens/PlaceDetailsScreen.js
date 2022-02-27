23cc76f8bea4eecdaacfc5366cfc0c1b2416041e
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-27 17:06:33 +0100

    PlaceDetails step 4

--- screens/PlaceDetailsScreen.js
+++ screens/PlaceDetailsScreen.js
@@ -11,11 +11,40 @@ function Banner({place}) {
     );
 }
 
+function PlaceProperty({value, title}) {
+    return (
+        <View style={stylesPlaceProp.container}>
+            <Text style={stylesPlaceProp.title}>{title}</Text>
+            <Text style={stylesPlaceProp.value}>{value}</Text>
+            <View style={[stylesPlaceProp.hairline, stylesPlaceProp.hairlineBare]}/>
+        </View>
+    );
+}
+
+// noinspection JSSuspiciousNameCombination
+const stylesPlaceProp = StyleSheet.create(
+    {
+        container: tw`bg-gray-100 m-2 pb-4 rounded-xl`,
+        title: tw`px-8 font-semibold text-gray-400 text-lg`,
+        value: tw`px-8`,
+        hairline: tw`mx-8`,
+        hairlineBare: {height: StyleSheet.hairlineWidth, backgroundColor: "gray"},
+    }
+)
+
 export function PlaceDetailsScreen({route}) {
     const {place} = route.params;
     return (
         <View style={styles.container}>
             <Banner place={place}/>
+            <PlaceProperty value={place.id} title="id"/>
+            <PlaceProperty value={place.name} title="name"/>
+            <PlaceProperty value={place.description} title="description"/>
+            <PlaceProperty value={place.info} title="info"/>
+            <PlaceProperty value={place.icon} title="icon"/>
+            <PlaceProperty value={place.color} title="color"/>
+            <PlaceProperty value={place.location.lat} title="latitude"/>
+            <PlaceProperty value={place.location.lng} title="longitude"/>
         </View>
     );
 }
@@ -24,6 +53,6 @@ const styles = StyleSheet.create(
     {
         container: tw`h-full`,
         banner: tw`p-5 flex-row bg-orange-200 `,
-        bannerText: tw`text-purple-900 font-bold text-6xl text-center text-lg flex-stretch`,
+        bannerText: tw`text-purple-900 font-bold text-6xl text-center text-lg`,
     }
 )
