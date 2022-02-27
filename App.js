286590100e1f71ced5c39d24b7cd4d33df2590b6
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-27 12:52:33 +0100

    add navigation style header

--- App.js
+++ App.js
@@ -4,18 +4,24 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NAV_HOME, NAV_PLACES} from "./navigation_constants";
 import {PlacesScreen} from "./screens/PlacesScreen";
+import tw from "twrnc";
 
 function ProvidedApp() {
     const Stack = createNativeStackNavigator();
     return (
         <>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={screenOptions}>
                 <Stack.Screen name={NAV_HOME} component={HomeScreen}/>
                 <Stack.Screen name={NAV_PLACES} component={PlacesScreen}/>
             </Stack.Navigator>
         </>
     );
 }
+
+const screenOptions = ({
+    headerStyle: tw`bg-orange-300`,
+    headerTitleStyle: tw`text-purple-900 text-xl font-bold`,
+});
 
 export default function App() {
     return (
